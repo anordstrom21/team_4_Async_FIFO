75387e1bb2330aa3844e4cb1e3100cb630cf7a4c
Alexander Maso <amaso@auto.ece.pdx.edu>
2024-04-24 20:19:53 -0700

    Another buffer added to sync... 3 buffers, 4 stage pipeline

--- M1/sandbox/second_attempt/project/sync.sv
+++ M1/sandbox/second_attempt/project/sync.sv
@@ -8,16 +8,19 @@ module sync #(
 
     logic [ADDR_WIDTH:0] buffer;
     logic [ADDR_WIDTH:0] buffer2;
+    logic [ADDR_WIDTH:0] buffer3;
 
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             data_out <= 0;
             buffer <= 0;
             buffer2 <= 0;
+            buffer3 <= 0;
         end else begin
             buffer <= data_in;
             buffer2 <= buffer;
-            data_out <= buffer2;
+            buffer3 <= buffer2;
+            data_out <= buffer3;
         end
     end
 
