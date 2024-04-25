bdfe80691632fe1458c9f0a9314f012063a7e3a9
Alexander Maso <amaso@auto.ece.pdx.edu>
2024-04-24 23:15:39 -0700

    moved the partially debugged code to second_attempt and out of the working area

--- M1/sandbox/second_attempt/project/sync.sv
+++ M1/sandbox/second_attempt/project/sync.sv
@@ -7,17 +7,14 @@ module sync #(
 );
 
     logic [ADDR_WIDTH:0] buffer;
-    logic [ADDR_WIDTH:0] buffer2;
 
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             data_out <= 0;
             buffer <= 0;
-            buffer2 <= 0;
         end else begin
             buffer <= data_in;
-            buffer2 <= buffer;
-            data_out <= buffer2;
+            data_out <= buffer;
         end
     end
 
