54f62ad3f53e8c6b56eea0fa579f8103193df5b6
Alexander Maso <amaso@mo.ece.pdx.edu>
2024-04-30 23:05:45 -0700

    Changed implementation for the ptrs and grey code encoding. Followed FIFO1 doc

--- M2/sandbox/read_pointer.sv
+++ M2/sandbox/read_pointer.sv
@@ -3,22 +3,31 @@ module read_pointer #(
 )(
     input  logic                    clk, rst_n, inc,
     input  logic [ADDR_WIDTH:0]     rq2_wptr,
-    output logic [ADDR_WIDTH:0]     rptr,
+    output logic [ADDR_WIDTH:0]     rptr, 
     output logic [ADDR_WIDTH-1:0]   raddr,
     output logic                    empty
 );
 
+    logic [ADDR_WIDTH:0]    gray_rptr_next;
+    logic [ADDR_WIDTH:0]    binary_rptr, binary_rptr_next;
+    logic                   empty_next;
+
     always_ff @(posedge clk or negedge rst_n) begin
-        if (!rst_n)
+        if (!rst_n) begin
             rptr <= 0;
-        else if (inc && !empty)
-            rptr <= rptr + 1;
+            binary_rptr <= 0;
+            empty <= 1;
+        end
+        else begin
+            rptr <= gray_rptr_next;
+            binary_rptr <= binary_rptr_next;
+            empty <= empty_next;
+        end
     end
 
-    always_ff @(posedge clk) begin
-        empty <= (rptr == rq2_wptr);
-    end
-
-    assign raddr = rptr[ADDR_WIDTH-1:0];
+    assign empty_next = (gray_rptr_next == rq2_wptr);
+    assign gray_rptr_next = (binary_rptr_next >> 1) ^ binary_rptr_next;
+    assign binary_rptr_next = binary_rptr + (inc & ~empty);
+    assign raddr = binary_rptr[ADDR_WIDTH-1:0];
 
 endmodule
--- M2/sandbox/write_pointer.sv
+++ M2/sandbox/write_pointer.sv
@@ -8,18 +8,31 @@ module write_pointer #(
     output logic                    full
 );
 
-    always_ff @(posedge clk or negedge rst_n) begin
-        if (!rst_n)
-            wptr <= 0;
-        else if (inc && !full)
-            wptr <= wptr + 1;
-    end
+    logic   [ADDR_WIDTH:0]  binary_wptr;
+    logic   [ADDR_WIDTH:0]  binary_wptr_next, gray_wptr_next;
+    logic                   full_next;
 
-    always_ff @(posedge clk) begin
-        full <= (wptr[ADDR_WIDTH-1:0] == wq2_rptr[ADDR_WIDTH-1:0]) && (wptr[ADDR_WIDTH] != wq2_rptr[ADDR_WIDTH]);
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            wptr <= 0;
+            binary_wptr <= 0;
+            full <= 0;
+        end
+        else begin 
+            wptr <= gray_wptr_next;
+            binary_wptr <= binary_wptr_next;
+            full <= full_next;
+        end
     end
 
     assign waddr = wptr[ADDR_WIDTH-1:0];
+    assign binary_wptr_next = binary_wptr + (inc & ~full);
+    assign gray_wptr_next = (binary_wptr_next>>1) ^ binary_wptr_next;
+
+    assign full_next =  ((gray_wptr_next[ADDR_WIDTH-2:0] == wq2_rptr[ADDR_WIDTH-2:0]) &&
+                        (gray_wptr_next[ADDR_WIDTH-1:0] != wq2_rptr[ADDR_WIDTH-1:0]) && 
+                        (gray_wptr_next[ADDR_WIDTH] != wq2_rptr[ADDR_WIDTH]));
+
     
 endmodule
 
