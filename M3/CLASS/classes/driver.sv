51075e547aec6b8796d22ff89977a289d37f0761
Alexander Maso <amaso@mo.ece.pdx.edu>
2024-05-16 15:43:47 -0700

    In midst of splitting up tester -> generator + driver

--- M3/CLASS/classes/driver.sv
+++ M3/CLASS/classes/driver.sv
@@ -17,11 +17,20 @@ class driver;
     bfm = b;
   endfunction
 
+  extern function new(mailbox gen2driv);
+    this.gen2driv = gen2driv;
+  endfunction
+
   // internal signal to track address
   bit [ADDR_WIDTH-1:0]  address = 0;
 
   task execute();
     forever begin
+      transaction tx;
+      gen2driv.get(tx);
+      // I believe we want to call a write task here to drive tx into the bfm...
+      // Will need to code the write task below as well...
+
       @(posedge bfm.clk_wr);
       if (bfm.wr_en && !bfm.full) begin
         address++;
