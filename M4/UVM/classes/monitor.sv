e3d8d08e214c8e8ebd4f69577e6fbf4706e364ea
apmaso <amaso@pdx.edu>
2024-05-29 21:11:57 -0700

    removed seperate rd and wr ports and txs

--- M4/UVM/classes/monitor.sv
+++ M4/UVM/classes/monitor.sv
@@ -11,11 +11,10 @@ class fifo_monitor extends uvm_monitor;
   `uvm_component_utils(fifo_monitor) // Register the component with the factory
   
   virtual fifo_bfm bfm;
-  fifo_transaction tx_wr, tx_rd;
+  fifo_transaction tx;
 
   // Declare analysis port
-  uvm_analysis_port #(fifo_transaction) monitor_port_rd;
-  uvm_analysis_port #(fifo_transaction) monitor_port_wr;
+  uvm_analysis_port #(fifo_transaction) monitor_port;
 
   // This variable is used to determine if the last transaction was a read
   bit last_rd_en = 0;
@@ -36,8 +35,7 @@ class fifo_monitor extends uvm_monitor;
       `uvm_fatal("NOBFM", {"bfm not defined for ", get_full_name(), "."});
   
     // Use new constructor to create the analysis port
-    monitor_port_wr = new("monitor_port_wr", this);
-    monitor_port_rd = new("monitor_port_rd", this);
+    monitor_port = new("monitor_port_wr", this);
   endfunction : build_phase
   
   // Connect phase   TODO: Check if this can be virtual
@@ -63,16 +61,16 @@ class fifo_monitor extends uvm_monitor;
 
   task monitor_wr();
     forever begin
-      tx_wr = fifo_transaction::type_id::create("fifo_transaction");
+      tx = fifo_transaction::type_id::create("tx");
       if (bfm.wr_en) begin 
         @(posedge bfm.clk_wr);
-          tx_wr.wr_en = bfm.wr_en;
-          tx_wr.data_in = bfm.data_in;
-          tx_wr.empty = bfm.empty;
-          tx_wr.full = bfm.full;
-          tx_wr.half = bfm.half;
-          `uvm_info(get_type_name(), $sformatf("Monitor tx_wr \t|  wr_en: %b  |  rd_en: %b  |  data_in: %h  |  data_out: %h  |  full: %b  |  empty: %b  |  half: %b", tx_wr.wr_en, tx_wr.rd_en, tx_wr.data_in, tx_wr.data_out, tx_wr.full, tx_wr.empty, tx_wr.half), UVM_DEBUG);
-          monitor_port_wr.write(tx_wr);
+          tx.wr_en = bfm.wr_en;
+          tx.data_in = bfm.data_in;
+          tx.empty = bfm.empty;
+          tx.full = bfm.full;
+          tx.half = bfm.half;
+          `uvm_info(get_type_name(), $sformatf("Monitor tx \t|  wr_en: %b  |  rd_en: %b  |  data_in: %h  |  data_out: %h  |  full: %b  |  empty: %b  |  half: %b", tx.wr_en, tx.rd_en, tx.data_in, tx.data_out, tx.full, tx.empty, tx.half), UVM_DEBUG);
+          monitor_port.write(tx);
       end
     end
   endtask : monitor_wr
@@ -80,29 +78,29 @@ class fifo_monitor extends uvm_monitor;
   task monitor_rd();
     #((READ_DELAY+8)*CYCLE_TIME_RD); // wait for the driver to reset and for some data to be put on the FIFO (8 RD_CLK min...)
     forever begin
-      tx_rd = fifo_transaction::type_id::create("fifo_transaction");
+      tx = fifo_transaction::type_id::create("tx");
       if (bfm.rd_en) begin
         @(posedge bfm.clk_rd);
-          tx_rd.rd_en = bfm.rd_en;
+          tx.rd_en = bfm.rd_en;
           // If the last transaction was also a read, then we must wait for the next read clock edge
-          if (tx_rd.rd_en && last_rd_en) begin
+          if (tx.rd_en && last_rd_en) begin
             #(CYCLE_TIME_RD);
-            tx_rd.data_out = bfm.data_out;
-            tx_rd.empty = bfm.empty;
-            tx_rd.full = bfm.full;
-            tx_rd.half = bfm.half;
+            tx.data_out = bfm.data_out;
+            tx.empty = bfm.empty;
+            tx.full = bfm.full;
+            tx.half = bfm.half;
           end
           // Otherwise, we can sample the data_out on the same cycle as the read
           else begin
             @(posedge bfm.clk_rd);
-            tx_rd.data_out = bfm.data_out;
-            tx_rd.empty = bfm.empty;
-            tx_rd.full = bfm.full;
-            tx_rd.half = bfm.half;
+            tx.data_out = bfm.data_out;
+            tx.empty = bfm.empty;
+            tx.full = bfm.full;
+            tx.half = bfm.half;
           end
-          last_rd_en = tx_rd.rd_en;
-          `uvm_info(get_type_name(), $sformatf("Monitor tx_rd \t|  wr_en: %b  |  rd_en: %b  |  data_in: %h  |  data_out: %h  |  full: %b  |  empty: %b  |  half: %b", tx_rd.wr_en, tx_rd.rd_en, tx_rd.data_in, tx_rd.data_out, tx_rd.full, tx_rd.empty, tx_rd.half), UVM_DEBUG);
-          monitor_port_rd.read(tx_rd);
+          last_rd_en = tx.rd_en;
+          `uvm_info(get_type_name(), $sformatf("Monitor tx \t|  wr_en: %b  |  rd_en: %b  |  data_in: %h  |  data_out: %h  |  full: %b  |  empty: %b  |  half: %b", tx.wr_en, tx.rd_en, tx.data_in, tx.data_out, tx.full, tx.empty, tx.half), UVM_DEBUG);
+          monitor_port.write(tx);
       end
     end
   endtask : monitor_rd 
@@ -112,28 +110,28 @@ class fifo_monitor extends uvm_monitor;
     #((READ_DELAY+8)*CYCLE_TIME_RD); // wait for the driver to reset and for some data to be put on the FIFO (8 RD_CLK min...)
     $display("********** Monitor Started **********"); 
     repeat(TX_COUNT_RD) begin
-      gen2mon.get(tx_rd);
+      gen2mon.get(tx);
       @(posedge bfm.clk_rd);
-        bfm.rd_en <= tx_rd.rd_en;
+        bfm.rd_en <= tx.rd_en;
         // If the last transaction was also a read, then we must wait for the next read clock edge
-        if (tx_rd.rd_en && last_rd_en) begin
+        if (tx.rd_en && last_rd_en) begin
           #(CYCLE_TIME_RD);
-          tx_rd.data_out = bfm.data_out;
-          tx_rd.empty = bfm.empty;
-          tx_rd.full = bfm.full;
-          tx_rd.half = bfm.half;
+          tx.data_out = bfm.data_out;
+          tx.empty = bfm.empty;
+          tx.full = bfm.full;
+          tx.half = bfm.half;
         end
         // Otherwise, we can sample the data_out on the same cycle as the read
         else begin
           @(posedge bfm.clk_rd);
-          tx_rd.data_out = bfm.data_out;
-          tx_rd.empty = bfm.empty;
-          tx_rd.full = bfm.full;
-          tx_rd.half = bfm.half;
+          tx.data_out = bfm.data_out;
+          tx.empty = bfm.empty;
+          tx.full = bfm.full;
+          tx.half = bfm.half;
         end
-        last_rd_en = tx_rd.rd_en;
-        mon2scb.put(tx_rd);
-        $display("Monitor tx_rd \t|  wr_en: %b  |  rd_en: %b  |  data_in: %h  |  data_out: %h  |  full: %b  |  empty: %b  |  half: %b", tx_rd.wr_en, tx_rd.rd_en, tx_rd.data_in, tx_rd.data_out, tx_rd.full, tx_rd.empty, tx_rd.half); 
+        last_rd_en = tx.rd_en;
+        mon2scb.put(tx);
+        $display("Monitor tx \t|  wr_en: %b  |  rd_en: %b  |  data_in: %h  |  data_out: %h  |  full: %b  |  empty: %b  |  half: %b", tx.wr_en, tx.rd_en, tx.data_in, tx.data_out, tx.full, tx.empty, tx.half); 
     end
     $display("********** Monitor Ended **********"); 
   endtask : execute
--- M4/UVM/classes/scoreboard.sv
+++ M4/UVM/classes/scoreboard.sv
@@ -13,9 +13,8 @@ class fifo_scoreboard extends uvm_scoreboard;
 	`uvm_component_utils(fifo_scoreboard); // Register the component with the factory
 
     // Declare analysis port
-    uvm_analysis_imp #(fifo_transaction, fifo_scoreboard) scoreboard_port_rd;
-    uvm_analysis_imp #(fifo_transaction, fifo_scoreboard) scoreboard_port_wr;
-    fifo_transaction tx_rd, tx_wr;
+    uvm_analysis_imp #(fifo_transaction, fifo_scoreboard) scoreboard_port;
+    fifo_transaction tx;
 
     // Local memory, ptrs and count used to check FIFO
     localparam DEPTH = 2**ADDR_WIDTH;
@@ -37,8 +36,7 @@ class fifo_scoreboard extends uvm_scoreboard;
         `uvm_info(get_type_name(), $sformatf("Building %s", get_full_name()), UVM_HIGH);
 
         // Use new constructor to create the analysis ports
-        scoreboard_port_rd = new("scoreboard_port_rd", this);
-        scoreboard_port_wr = new("scoreboard_port_wr", this);
+        scoreboard_port = new("scoreboard_port", this);
     endfunction: build_phase
 
     // Connect phase
@@ -56,26 +54,21 @@ class fifo_scoreboard extends uvm_scoreboard;
     endtask: run_phase
 
 
-    task write(fifo_transaction tx_wr);
-        repeat(TX_COUNT_WR) begin
-            if (tx_wr.wr_en && !tx_wr.full) begin
-                scb_write(tx_wr.data_in);
-                `uvm_info(get_type_name(), $sformatf("Scoreboard tx_wr \t|  wr_en: %b  |  rd_en: %b  |  data_in: %h  |  data_out: %h", tx_wr.wr_en, tx_wr.rd_en, tx_wr.data_in, tx_wr.data_out), UVM_DEBUG);
+    function void write(fifo_transaction tx);
+        forever begin
+            if (tx.wr_en && !tx.full) begin
+                scb_write(tx.data_in);
+                `uvm_info(get_type_name(), $sformatf("Scoreboard tx \t|  wr_en: %b  |  rd_en: %b  |  data_in: %h  |  data_out: %h", tx.wr_en, tx.rd_en, tx.data_in, tx.data_out), UVM_DEBUG);
             end
+            else if (tx.rd_en && !tx.empty) begin
+                read_and_check(tx.data_out);
+                `uvm_info(get_type_name(), $sformatf("Scoreboard tx \t|  wr_en: %b  |  rd_en: %b  |  data_in: %h  |  data_out: %h", tx.wr_en, tx.rd_en, tx.data_in, tx.data_out), UVM_DEBUG);
+            end 
         end
-    endtask : write
-
-    task read(fifo_transaction tx_rd);
-        repeat(TX_COUNT_RD) begin
-            if (tx_rd.rd_en && !tx_rd.empty) begin
-                read_and_check(tx_rd.data_out);
-                `uvm_info(get_type_name(), $sformatf("Scoreboard tx_rd \t|  wr_en: %b  |  rd_en: %b  |  data_in: %h  |  data_out: %h", tx_rd.wr_en, tx_rd.rd_en, tx_rd.data_in, tx_rd.data_out), UVM_DEBUG);    
-            end
-        end
-    endtask : read
+    endfunction : write
 
     
-    task scb_write(input logic [DATA_WIDTH-1:0] data);
+    function void scb_write(input logic [DATA_WIDTH-1:0] data);
         if (count < DEPTH) begin
             memory[write_ptr] = data;
             write_ptr = (write_ptr + 1) % DEPTH; //Modulo keeps values in range from 0 to DEPTH-1
@@ -83,9 +76,9 @@ class fifo_scoreboard extends uvm_scoreboard;
         end else begin
             `uvm_error("SCOREBOARD", "Write to full FIFO attempted.");
         end
-    endtask : scb_write
+    endfunction : scb_write
   
-    task read_and_check(input logic [DATA_WIDTH-1:0] data);
+    function void read_and_check(input logic [DATA_WIDTH-1:0] data);
         if (count > 0) begin
             logic [DATA_WIDTH-1:0] expected_data = memory[read_ptr];
             if (data != expected_data) begin
@@ -99,7 +92,7 @@ class fifo_scoreboard extends uvm_scoreboard;
         end else begin
             `uvm_error("SCOREBOARD", "Read from empty FIFO attempted.");
         end
-    endtask : read_and_check
+    endfunction : read_and_check
 
      
 endclass 
